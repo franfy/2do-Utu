7f17eca755b61417781bcfc554b3ea3a84eea420
FrancoFaccelli <56889679+franfy@users.noreply.github.com>
2025-08-01 20:09:06 -0300

    Add files via upload

---
+++ Práctico II - Ventana con Layout/Gestor.java
@@ -0,0 +1,14 @@
+package Componentesdelaventana;
+
+public class Gestor {
+
+	public static void main(String[] args) {
+		//ej1 ventana = new ej1();
+		//ej2 ventana = new ej2();
+		ej3 ventana = new ej3();
+		ventana.setVisible(true);
+		
+		
+	}
+
+}
